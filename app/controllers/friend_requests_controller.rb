5c066eb2eb0235f47a93130b77f0ee6926fcc583
DamirSvrtan <damir.svrtan@gmail.com>
2013-03-05 02:46:13 +0100

    before deleting friend requests

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -4,7 +4,7 @@ class FriendRequestsController < ApplicationController
 	end
 
 	def create
-			@request=FriendRequest.new(:requester_id => current_user.id, :requestee_id => params[:friend_request][:requestee_id])
+			@request=FriendRequest.new(params[:friend_request])
 			@request.save!
 			redirect_to User.find(params[:friend_request][:requestee_id])
 
