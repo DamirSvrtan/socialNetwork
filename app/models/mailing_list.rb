174a62e215cfa85d7d70fccd3e84609cc0fd062c
DamirSvrtan <damir.svrtan@gmail.com>
2013-04-12 01:45:25 +0200

    Removed invalid records from the mailing list

--- app/models/mailing_list.rb
+++ app/models/mailing_list.rb
@@ -3,5 +3,5 @@ class MailingList < ActiveRecord::Base
   validates :email, :presence => true
   validates :email, :uniqueness => true
 
-
+  scope :invalid_email, select { |ml| ml.email !~ /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
