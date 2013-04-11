1db1a1aec0874e127fb556447f0e8238a1d53bc1
DamirSvrtan <damir.svrtan@gmail.com>
2013-04-11 19:01:52 +0200

    deleted g20

--- app/controllers/mailing_lists_controller.rb
+++ app/controllers/mailing_lists_controller.rb
@@ -15,7 +15,11 @@ end
 def email_exists
 	@mail = MailingList.find_by_email(params[:email])
 	respond_to do |format|
-		format.json { render :json => @mail}
+		if @mail
+			format.json { render :json => @mail}
+		else
+			format.json { render :json => { :email => nil }}
+		end
 	end
 end
 
